1030d0029ef9dbfb54a1dab99592de7585c698db
WiitterSimithYU <378961614@qq.com>
2018-05-18 08:36:33 +0800

    commit

---
+++ HHModuleDemo/HoverLinkageViewDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HoverLinkageViewDemo
+//
+//  Created by hxw on 2017/5/10.
+//  Copyright © 2017年 hxw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "HHAppDelegate.h"
+
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HHAppDelegate class]));
+    }
+}
